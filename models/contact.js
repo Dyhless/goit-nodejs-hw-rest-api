3cae0a3a49d1b115ec016f7c5e43be7de0aed595
Dmytro Medvediev <dsisitkin@gmail.com>
2023-09-26 17:54:58 +0200

    update 5th task

--- models/contact.js
+++ models/contact.js
@@ -1,26 +1,44 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const { handleMongooseError } = require("../helpers/handleMongooseError");
+const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: [true, "Set phone for contact"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-}, { versionKey: false, timestamps: true });
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleMongooseError);
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favotite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = { addSchema, updateFavoriteSchema };
+
 const Contact = model("contact", contactSchema);
 
-model.exports = Contact;
+module.exports = { Contact, schemas };
