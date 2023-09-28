1e8675c8ad08e33e0819d6893d8997abad0d45b1
Dmytro Medvediev <dsisitkin@gmail.com>
2023-09-28 14:51:42 +0200

    update 5th task

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,15 +1,15 @@
-const Contact = require("../models/contact");
+const {Contact} = require("../models/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.Contact.find({}, "-createdAt -updatedAt");
+  const result = await Contact.find({}, "-createdAt -updatedAt");
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.Contact.findById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -17,13 +17,13 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await Contact.Contact.create(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -32,16 +32,18 @@ const updateById = async (req, res) => {
 
 const updateFavorite = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+  const { favorite } = req.body; 
+  const result = await Contact.findByIdAndUpdate(contactId, { favorite }, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
   res.json(result);
 };
 
+
 const deleteById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.Contact.findByIdAndRemove(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw HttpError(404, "Not found");
   }
